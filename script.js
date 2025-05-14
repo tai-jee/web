0704bae803de3f0099a0311bb230ccab0ae2e7f2
tai-jee <itsme@taijee.xyz>
2025-05-15 00:26:19 +0100

    somewhat of a home page

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-// replace any elements with the class tai with the image img/tai-optimised.svg
-function replaceTai() {
-    var tai = document.getElementsByClassName("tai");
-    for (var i = 0; i < tai.length; i++) {
-        tai[i].src = "img/tai-optimised.svg";
+async function updateStatus() {
+    try {
+        fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://pastebin.com/raw/RS2K4xL3')}`)
+        .then(response => {
+            if (response.ok) return response.json()
+            throw new Error('Network response was not ok.')
+        })
+        .then(data => {
+            const statusElements = document.querySelectorAll('.status');
+            console.log(statusElements);
+            const statuses = data.contents.split("\n");
+            statusElements.forEach(element => {
+                var status = statuses[Math.floor((Math.random() * statuses.length))]
+                element.innerHTML = (status == "\\" ? "" : `"${status}"`);
+            });
+        });
+
+
+    } catch (error) {
+        console.error('Could not fetch or update status:', error);
+        const statusElements = document.querySelectorAll('.status');
+        statusElements.forEach(element => {
+            element.innerHTML = "Status Unavailable";
+        });
     }
 }
+
+function _calculateAge(birthday) { // birthday is a date
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+document.getElementById("age").innerHTML = _calculateAge(new Date("2008-01-25T22:55"))
+
+updateStatus();
